ef79dad5b4c9f062efd49760e5ad6361a3a62e4b
piotr-yuxuan <pierre.deboisset@telecom-paristech.fr>
2015-07-22 10:44:48 +0300

    mai4 (not working yet)

--- clojure-newbie/src/clojure_newbie/core.clj
+++ clojure-newbie/src/clojure_newbie/core.clj
@@ -1,7 +1,6 @@
 (ns clojure-newbie.core
   (:gen-class)
-  (:refer-clojure)
-  (require [clojure.set :as set]))
+  (:refer-clojure))
 (def filename "suspects.csv")
 
 (defn -main
@@ -201,226 +200,68 @@
   [{:name "a" :glitter-index 3}
    {:name "b" :glitter-index 4}])
 
-;; Chapter V
-(declare successful-move prompt-move game-over query-rows)
+;; Chapter V exercice 1
+(defn attr
+  [field]
+  (comp field :attributes))
 
-(defn tri*
-  "Generates lazy sequence of triangular numbers"
-  ([] (tri* 0 1))
-  ([sum n]
-   (let [new-sum (+ sum n)]
-     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+;; Chapter V exercice 2
+(defn my-comp
+  "Implement built-in comp"
+  [& functions]
+  (fn [operands]
+    (reduce #(%2 %)
+            operands
+            (reverse functions))))
 
-(def tri (tri*))
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
 
-(defn triangular?
-  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
-  [n]
-  (= n (last (take-while #(>= n %) tri))))
+;; Chapter V exercice 3
+(defn my-assoc-in
+  ;; Is it idiomatic?
+  [m [k & ks] v]
+  (loop [m m
+         k k
+         remaining ks]
+    (if (= 1 (count ks))
+      (assoc m ks v)
+      (let [[k1 & ks1] ks]
+        (recur (assoc m k nil)
+               k1
+               ks1)))))
 
-(defn row-tri
-  "The triangular number at the end of row n"
-  [n]
-  (last (take n tri)))
+(defn my-assoc-in2
+  ;; Is it idiomatic to have implicit recurrence?
+  [m [k & ks] v]
+  (assoc (if (= 1 (count ks))
+           m
+           (my-assoc-in2 m ks v))
+    ks v))
 
-(defn row-num
-  "Returns row number the position belongs to: pos 1 in row 1,
-  positions 2 and 3 in row 2, etc"
-  [pos]
-  (inc (count (take-while #(> pos %) tri))))
+(defn mai3
+  "Implement built-in assoc-in"
+  [m [k & ks] v]
+  (str m)
+  (let [v1 (if (contains? m k)
+             (mai3 (get m k) ks v)
+             v)]
+    (assoc m k v1)))
 
-(defn connect
-  "Form a mutual connection between two positions"
-  [board max-pos pos neighbor destination]
-  (if (<= destination max-pos)
-    (reduce (fn [new-board [p1 p2]]
-              (assoc-in new-board [p1 :connections p2] neighbor))
-            board
-            [[pos destination] [destination pos]])
-    board))
-
-(defn connect-right
-  [board max-pos pos]
-  (let [neighbor (inc pos)
-        destination (inc neighbor)]
-    (if-not (or (triangular? neighbor) (triangular? pos))
-      (connect board max-pos pos neighbor destination)
-      board)))
-(defn connect-down-left
-  [board max-pos pos]
-  (let [row (row-num pos)
-        neighbor (+ row pos)
-        destination (+ 1 row neighbor)]
-    (connect board max-pos pos neighbor destination)))
-(defn connect-down-right
-  [board max-pos pos]
-  (let [row (row-num pos)
-        neighbor (+ 1 row pos)
-        destination (+ 2 row neighbor)]
-    (connect board max-pos pos neighbor destination)))
-
-(defn add-pos
-  "Pegs the position and performs connections"
-  [board max-pos pos]
-  (let [pegged-board (assoc-in board [pos :pegged] true)]
-    (reduce (fn [new-board connection-creation-fn]
-              (connection-creation-fn new-board max-pos pos))
-            pegged-board
-            [connect-right connect-down-left connect-down-right])))
-
-(defn new-board
-  "Creates a new board with the given number of rows"
-  [rows]
-  (let [initial-board {:rows rows}
-        max-pos (row-tri rows)]
-    (reduce (fn [board pos] (add-pos board max-pos pos))
-            initial-board
-            (range 1 (inc max-pos)))))
-
-(defn pegged?
-  "Does the position have a peg in it?"
-  [board pos]
-  (get-in board [pos :pegged]))
-
-(defn remove-peg
-  "Take the peg at given position out of the board"
-  [board pos]
-  (assoc-in board [pos :pegged] false))
-
-(defn place-peg
-  "Put a peg in the board at given position"
-  [board pos]
-  (assoc-in board [pos :pegged] true))
-
-(defn move-peg
-  "Take peg out of p1 and place it in p2"
-  [board p1 p2]
-  (place-peg (remove-peg board p1) p2))
-
-(defn valid-moves
-  "Return a map of all valid moves for pos, where the key is the
-  destination and the value is the jumped position"
-  [board pos]
-  (into {}
-        (filter (fn [[destination jumped]]
-                  (and (not (pegged? board destination))
-                       (pegged? board jumped)))
-                (get-in board [pos :connections]))))
-
-(defn valid-move?
-  "Return jumped position if the move from p1 to p2 is valid, nil
-  otherwise"
-  [board p1 p2]
-  (get (valid-moves board p1) p2))
-
-(defn make-move
-  "Move peg from p1 to p2, removing jumped peg"
-  [board p1 p2]
-  (if-let [jumped (valid-move? board p1 p2)]
-    (move-peg (remove-peg board jumped) p1 p2)))
-
-(defn can-move?
-  "Do any of the pegged positions have valid moves?"
-  [board]
-  (some (comp not-empty (partial valid-moves board))
-        (map first (filter #(get (second %) :pegged) board))))
-
-(def alpha-start 97)
-(def alpha-end 123)
-(def letters (map (comp str char) (range alpha-start alpha-end)))
-(def pos-chars 3)
-
-(defn render-pos
-  [board pos]
-  (str (nth letters (dec pos))
-       (if (get-in board [pos :pegged])
-         (colorize "0" :blue)
-         (colorize "-" :red))))
-
-(defn row-positions
-  "Return all positions in the given row"
-  [row-num]
-  (range (inc (or (row-tri (dec row-num)) 0))
-         (inc (row-tri row-num))))
-
-(defn row-padding
-  "String of spaces to add to the beginning of a row to center it"
-  [row-num rows]
-  (let [pad-length (/ (* (- rows row-num) pos-chars) 2)]
-    (apply str (take pad-length (repeat " ")))))
-
-(defn render-row
-  [board row-num]
-  (str (row-padding row-num (:rows board))
-       (clojure.string/join " " (map (partial render-pos board)
-                                     (row-positions row-num)))))
-
-(defn print-board
-  [board]
-  (doseq [row-num (range 1 (inc (:rows board)))]
-    (println (render-row board row-num))))
-
-(defn letter->pos
-  "Converts a letter string to the corresponding position number"
-  [letter]
-  (inc (- (int (first letter)) alpha-start)))
-
-(defn get-input
-  "Waits for user to enter text and hit enter, then cleans the input"
-  ([] (get-input nil))
-  ([default]
-   (let [input (clojure.string/trim (read-line))]
-     (if (empty? input)
-       default
-       (clojure.string/lower-case input)))))
-
-(defn prompt-move
-  [board]
-  (println "\nHere's your board:")
-  (print-board board)
-  (println "Move from where to where? Enter two letters:")
-  (let [input (map letter->pos (characters-as-strings (get-input)))]
-    (if-let [new-board (make-moveu board (first input) (second input))]
-      (user-entered-valid-move new-board)
-      (user-entered-invalid-move board))))
-
-(defn user-entered-invalid-move
-  "Handles the next step after a user has entered an invalid move"
-  [board]
-  (println "\n!!! That was an invalid move :(\n")
-  (prompt-move board))
-
-(defn user-entered-valid-move
-  "Handles the next step after a user has entered a valid move"
-  [board]
-  (if (can-move? board)
-    (prompt-move board)
-    (game-over board)))
-
-(defn game-over
-  "Announce the game is over and prompt to play again"
-  [board]
-  (let [remaining-pegs (count (filter :pegged (vals board)))]
-    (println "Game over! You had" remaining-pegs "pegs left:")
-    (print-board board)
-    (println "Play again? y/n [y]")
-    (let [input (get-input "y")]
-      (if (= "y" input)
-        (prompt-rows)
-        (do
-          (println "Bye!")
-          (System/exit 0))))))
-
-(defn prompt-empty-peg
-  [board]
-  (println "Here's your board:")
-  (print-board board)
-  (println "Remove which peg? [e]")
-  (prompt-move (remove-peg board (letter->pos (get-input "e")))))
-
-(defn prompt-rows
-  []
-  (println "How many rows? [5]")
-  (let [rows (Integer. (get-input 5))
-        board (new-board rows)]
-    (prompt-empty-peg board)))
+(defn mai4
+  ""
+  [map keys value]
+  (loop [[key & keys] (reverse keys)
+         final map
+         initial (assoc {} key value)]
+    (if (not (empty? keys))
+      (let [keys (reverse keys)]
+        (println "key:" key)
+        (println "rem:" keys)
+        (recur keys
+               final
+               (assoc {} key initial)))
+      (assoc final key initial))))
